40ffea0ecd170b8a934e3b9434a4975f307095dc
lilholmen <lilholmen@yndex.ru>
2022-04-10 01:49:09 +0300

    hero card progress done: attributes, portrait, name, ascention/stars, furniture; in-progress: gear, si, engraving, better design

--- js/selected-hero.js
+++ js/selected-hero.js
@@ -4,22 +4,28 @@ export function card() {
   const container = document.querySelector('.selected-hero');
   const currentHero = mergedHeroes.find((item) => item.id === this.id);
 
+  const capitalFirstLetter = function (str) {
+    str = str.split('');
+    str[0] = str[0].toUpperCase();
+
+    return str.join('');
+  };
+
   console.table(currentHero);
   container.classList.remove('selected-hero--none');
 
   const selectedHero = {
-    name: container.querySelector('.selected-hero__name'),
-    ascention: container.querySelector('.selected-hero__ascention'),
-    faction: container.querySelector('.selected-hero__faction'),
-    base: container.querySelector('.selected-hero__base'),
-    class: container.querySelector('.selected-hero__class'),
-    portrait: container.querySelector('.selected-hero__portrait'),
+    name: container.querySelector('.static__name'),
+    ascention: container.querySelector('.ascention__level'),
+    faction: container.querySelector('.attributes__image--faction'),
+    base: container.querySelector('.attributes__image--base'),
+    class: container.querySelector('.attributes__image--class'),
+    portrait: container.querySelector('.portrait-area__image'),
     furniture: container.querySelector('.selected-hero__furniture'),
-    stars: container.querySelector('.selected-hero__stars'),
+    stars: container.querySelector('.ascention__stars-area'),
   };
 
-  selectedHero.name.textContent = currentHero.name;
-  selectedHero.ascention.textContent = currentHero.ascention;
+  selectedHero.name.textContent = capitalFirstLetter(currentHero.name);
   selectedHero.faction.src = `img/params/faction_${currentHero.mask[0]}.webp`;
   selectedHero.base.src = `img/params/base_${currentHero.mask[1]}.png`;
   selectedHero.class.src = `img/params/class_${currentHero.mask[2]}.png`;
@@ -27,23 +33,31 @@ export function card() {
 
   selectedHero.stars.innerHTML = '';
   if (currentHero.stars) {
+    selectedHero.ascention.innerHTML = '';
+
     for (let i = 1; i <= currentHero.stars; i++) {
       const star = document.createElement('img');
 
-      star.classList.add('ascention__star');
+      star.classList.add('stars-area__image');
       star.src = 'img/params/ascention_star.png';
 
       selectedHero.stars.append(star);
     }
+  } else {
+    selectedHero.ascention.textContent = currentHero.ascention;
   }
 
   selectedHero.furniture.innerHTML = '';
   if (currentHero.furniture) {
-    for (let i = 1; i <= currentHero.furniture; i++) {
+    for (let i = 0; i < 9; i++) {
       const fur = document.createElement('img');
 
-      fur.classList.add('furniture__mythic');
-      fur.src = 'img/params/furniture_0.png';
+      fur.classList.add('furniture__gem');
+      if (i < currentHero.furniture) {
+        fur.src = 'img/params/fur.png';
+      } else {
+        fur.src = 'img/params/fur_leg.png';
+      }
 
       selectedHero.furniture.append(fur);
     }
